75042b99c043e02602c83aa1de7a98a80b8a6cae
Avinash Kumar <37548802+rock12231@users.noreply.github.com>
2024-04-13 05:30:41 +0530

    db code

--- HM_API/settings.py
+++ HM_API/settings.py
@@ -105,11 +105,7 @@ WSGI_APPLICATION = 'HM_API.wsgi.application'
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/HM_API',
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
 REST_FRAMEWORK = {
